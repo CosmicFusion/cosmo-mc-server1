fdb142975c4439e51d1e8555980d2add4f171708
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-05-13 13:26:22 +0000

    Cron save

--- keep_server_alive.sh
+++ keep_server_alive.sh
@@ -7,15 +7,15 @@ do
     echo "current uptime: $SERVER_UPTIME"
     # Minecraft Text
     echo "Injecting text into minecraft server"
-    ./server-command.sh say This a 24/7 Minecraft Server Please Do not Shutoff github codespaces
-    ./server-command.sh "say current uptime: $SERVER_UPTIME"
+    ./server_command.sh say This a 24/7 Minecraft Server Please Do not Shutoff github codespaces
+    ./server_command.sh "say current uptime: $SERVER_UPTIME"
     # Backup Notice
     echo "Saving World Right Now!"
-    ./server-command.sh say Doing Backup Right Now!
-    ./server-command save-all
-    ./server.sh say Save Done!
+    ./server_command.sh say Doing Backup Right Now!
+    ./server_command.sh save-all
+    ./server_command.sh say Save Done!
     echo "Saving Changes to git!"
-    ./server-command.sh say Saving Changes to git!
+    ./server_command.sh say Saving Changes to git!
     git add . && git commit -a -m "Cron save" && git push
     sleep 2h
 done
